bfdb5387f502527d386823abd33b8cf20b16e734
darling-flag-8189 <dwinniford@gmail.com>
2019-12-14 02:16:33 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,7 +5,7 @@ def directors_totals(nds)
   result = {
   }
   outer_index = 0 
-  while outer_index < directors_database[outer_index].count do 
+  while outer_index < directors_database.count do 
     inner_index = 0 
     movies_total = 0 
     while inner_index < directors_database[outer_index][:movies].count do 
